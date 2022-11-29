99a3390cfd8f86f7043ab083bd4deb02245fac89
Carl Pearson <cjp.uxr@gmail.com>
2022-11-29 13:00:02 -0600

    Create cjp_r_helpers.r

---
+++ cjp_r_helpers.r
@@ -0,0 +1,52 @@
+to.clipboard <- function(x){ 
+  clipr::write_clip(x)
+  }
+  
+  #custom theming
+theme_cjp <- function(){ 
+  #font <- "Inter"   #assign font family up front
+  
+  theme_minimal() %+replace%    #replace elements we want to change
+    
+    theme(
+      
+      #grid elements
+      panel.grid.major = element_blank(),    #strip major gridlines
+      panel.grid.minor = element_blank(),    #strip minor gridlines
+      #axis.ticks.x = element_blank(),          #strip axis ticks
+      
+      #since theme_minimal() already strips axis lines, 
+      #we don't need to do that again
+      
+      #text elements
+      text = element_text(family="Inter",color = "#040C4A"),
+      axis.text = element_text(color = "#040C4A")
+    )
+}
+
+
+
+#read all excel sheets
+read_excel_allsheets <- function(filename, tibble = FALSE) {
+  # I prefer straight data.frames
+  # but if you like tidyverse tibbles (the default with read_excel)
+  # then just pass tibble = TRUE
+  sheets <- readxl::excel_sheets(filename)
+  x <- lapply(sheets, function(X) readxl::read_excel(filename, sheet = X))
+  if(!tibble) x <- lapply(x, as.data.frame)
+  names(x) <- sheets
+  x
+}
+
+#insert new row
+insertRow <- function(existingDF, newrow, r) {
+  existingDF[seq(r+1,nrow(existingDF)+1),] <- existingDF[seq(r,nrow(existingDF)),]
+  existingDF[r,] <- newrow
+  existingDF
+}
+
+#opposite of intersect w/ dplyr
+outersect <- function(x, y) {
+  sort(c(x[!x%in%y],
+         y[!y%in%x]))
+}
