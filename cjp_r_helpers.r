93ecbdbe8abf325dccb15cc95023d68a8a6f9421
Carl Pearson <cjp.uxr@gmail.com>
2023-06-12 11:51:52 -0500

    Update cjp_r_helpers.r

--- cjp_r_helpers.r
+++ cjp_r_helpers.r
@@ -85,6 +85,8 @@ coord_100pc <- coord_cartesian(ylim=c(0,1))
 rc.gray <- c("#7A9299")
 rc.2.gray <- c("#BBC7CC","#7A9299")
 rc.5.color <- c("#FFD635","#FFA800","#FF4500","#00CCC0","#02315C")
+              
+rc.6.color <- c("#FFD635","#FFA800","#FF4500","#00CCC0",#FFF8B8,"#02315C")
 
 # http://www.cookbook-r.com/Graphs/Colors_(ggplot2)/
 cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
@@ -94,11 +96,11 @@ cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2",
               
 ##logit to probably - src: https://sebastiansauer.github.io/convert_logit2prob/
               
-logit2prob <- function(logit){
-  odds <- exp(logit)
-  prob <- odds / (1 + odds)
-  return(prob)
-}
+#logit2prob <- function(logit){
+#  odds <- exp(logit)
+#  prob <- odds / (1 + odds)
+#  return(prob)
+#}
 
 #not IN function  
 '%!in%' <- function(x,y)!('%in%'(x,y))         
