50decf68e9009008f979c193cd0dbb291c0c80d7
Carl Pearson <cjp.uxr@gmail.com>
2024-11-07 09:18:07 -0600

    Update cjp_r_helpers.r

--- cjp_r_helpers.r
+++ cjp_r_helpers.r
@@ -1,5 +1,32 @@
 
 ##############################
+###### set up   #####
+##############################
+
+
+##### get packages
+#### source: https://vbaliga.github.io/posts/2019-04-28-verify-that-r-packages-are-installed-and-loaded/
+
+## If a package is installed, it will be loaded. If any 
+## are not, the missing package(s) will be installed 
+## from CRAN and then loaded.
+
+## First specify the packages of interest
+packages = c("readxl", "dplyr","RCurl","ggplot2","DescTools")
+
+## Now load or install&load all
+package.check <- lapply(
+  packages,
+  FUN = function(x) {
+    if (!require(x, character.only = TRUE)) {
+      install.packages(x, dependencies = TRUE)
+      library(x, character.only = TRUE)
+    }
+  }
+)
+
+
+##############################
 ###### data read_write   #####
 ##############################
 
